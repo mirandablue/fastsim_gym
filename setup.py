e48d0b3af8bcc92ffaf0227daf33c8b3e3a3469d
Alex Coninx <alex@coninx.org>
2019-07-16 14:15:22 +0200

    Completed setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(name='gym_fastsim',
-      version='0.0.1',
-      install_requires=['gym>=0.2.3','pyfastsim']
+      version='0.0.2',
+      install_requires=['gym>=0.2.3','pyfastsim'],
+      packages=['gym_fastsim'],
+      author='Alex Coninx',
+      author_email='coninx@isir.upmc.fr'
 )
