e5255448febfb6645adb9adecb7124464e714788
Alexandre Coninx <alexandre.coninx@isir.upmc.fr>
2019-07-16 12:16:41 +0200

    Remove unneeded Bullet dependancy

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_fastsim',
       version='0.0.1',
-      install_requires=['gym>=0.2.3','pybullet','pyfastsim']
+      install_requires=['gym>=0.2.3','pyfastsim']
 )
