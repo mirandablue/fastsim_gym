4f556309237094b3deaf3353e8379bfab2905a52
Alexandre Coninx <alexandre.coninx@isir.upmc.fr>
2019-07-16 12:09:22 +0200

    Change sticky walls default

--- gym_fastsim/simple_nav/nav_env.py
+++ gym_fastsim/simple_nav/nav_env.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 default_env = "assets/LS_maze_hard.xml"
 #default_env = "assets/example.xml"
 
-sticky_walls = True # default is True
+sticky_walls = False # default is True in libfastsim... but false in the fastsim sferes2 module -> stick (haha) with that
 
 
 def sqdist(x,y):
