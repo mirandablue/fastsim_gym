1c7213ce2667113a88adff185280c9b840d80a2b
doncieux <stephane.doncieux@sorbonne-universite.fr>
2022-01-17 11:28:15 +0100

    Added a restart from a given position

--- example/example_gym_fastsim.py
+++ example/example_gym_fastsim.py
@@ -12,6 +12,7 @@ import time
 display= True
 
 env = gym.make('FastsimSimpleNavigation-v0')
+print(" == Reset to the default initial position ==")
 env.reset()
 action=[10,11]
 
@@ -20,7 +21,7 @@ if(display):
 
 then = time.time()
 
-for i in range(10000):
+for i in range(100):
         env.render()
         o,r,eo,info=env.step(action)
         print("Step %d Obs=%s  reward=%f  dist. to objective=%f  robot position=%s  End of ep=%s" % (i, str(o), r, info["dist_obj"], str(info["robot_pos"]), str(eo)))
@@ -28,6 +29,21 @@ for i in range(10000):
         	time.sleep(0.01)
         if eo:
             break
+
+pos=[55, 55, 0]
+print(" == Reset to a position given as argument: "+str(pos)+" ==")
+# reset from a position given as argument instead of the default position
+env.reset2(pos)
+
+for i in range(100):
+        env.render()
+        o,r,eo,info=env.step(action)
+        print("Step %d Obs=%s  reward=%f  dist. to objective=%f  robot position=%s  End of ep=%s" % (i, str(o), r, info["dist_obj"], str(info["robot_pos"]), str(eo)))
+        if(display):
+        	time.sleep(0.01)
+        if eo:
+            break
+
 
 now = time.time()
 
